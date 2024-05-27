77831754d0024e9ce9fc3e74e230165a0fd5a14f
NicoLucentini <lucentini.nicolas@gmail.com>
2024-05-27 15:30:11 -0300

    Fix code

--- src/main/java/org/example/controller/InmoupController.java
+++ src/main/java/org/example/controller/InmoupController.java
@@ -2,23 +2,23 @@ package org.example.controller;
 
 import org.example.services.InmoupService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/inmoup")
 public class InmoupController {
 
     @Autowired
     private InmoupService inmoupService;
 
     @GetMapping("/casas")
-    public void getCasas(){
-        inmoupService.getCasas();
+    public ResponseEntity<String> getCasas(){
+        return ResponseEntity.ok().body(inmoupService.getCasas());
     }
     @GetMapping("/searchAmount/{value}")
-    public void searchAmount(@PathVariable("value") long value){
+    public ResponseEntity<String> searchAmount(@PathVariable("value") long value){
         inmoupService.changeAmount(value);
+        return ResponseEntity.ok().body("Changed to "+ value);
     }
 }
--- src/main/java/org/example/services/InmoupService.java
+++ src/main/java/org/example/services/InmoupService.java
@@ -14,65 +14,33 @@ import static java.lang.Integer.valueOf;
 public class InmoupService {
     private int searchValue = 2;
 
-    private String CASAS_URL = "https://www.inmoup.com.ar/inmuebles/casas-en-venta?favoritos=0&limit=" + searchValue +"&prevEstadoMap=&q=Mendoza&lastZoom=13&precio%5Bmin%5D=&precio%5Bmax%5D=&moneda=1&sup_cubierta%5Bmin%5D=&sup_cubierta%5Bmax%5D=&sup_total%5Bmin%5D=&sup_total%5Bmax%5D=";
+    private String CASAS_URL() {
+        return "https://www.inmoup.com.ar/inmuebles/casas-en-venta?favoritos=0&limit=" + searchValue +"&prevEstadoMap=&q=Mendoza&lastZoom=13&precio%5Bmin%5D=&precio%5Bmax%5D=&moneda=1&sup_cubierta%5Bmin%5D=&sup_cubierta%5Bmax%5D=&sup_total%5Bmin%5D=&sup_total%5Bmax%5D=";
+    }
 
-    public void getCasas(){
+    public String getCasas(){
 
         RestTemplate rest = new RestTemplate();
 
-        String response = rest.getForObject(CASAS_URL, String.class);
+        String response = rest.getForObject(CASAS_URL(), String.class);
 
-        Integer amountFromPage = getAmount(response);
+        System.out.println("Checking url: "+ CASAS_URL()    );
+
         String finalString = getPropiedadesFromResponse(response);
-        finalString = cleanJsonToMakeItArray(finalString, amountFromPage);
 
-        // Creating a File object that
-        // represents the disk file
-        try
-        {
-            PrintStream fileStream = new PrintStream(new File("Props.txt"));
 
-            System.setOut(fileStream);
 
-            System.out.println(finalString);
-        }
-        catch (Exception e){}
+        return finalString;
 
-    }
-
-    private Integer getAmount(String response){
-        var split1 = response.split("h2");
-        var lotes = split1[1].split("h2");
-        String regex = "\\d+";
-        Pattern pattern = Pattern.compile(regex);
-        Pattern p = Pattern.compile("\\d+");
-        Matcher m = p.matcher(lotes[0]);
-        while(m.find()) {
-            return valueOf(m.group());
-        }
-        return -1;
     }
     private String getPropiedadesFromResponse(String response){
         var split = response.split("propiedades = ");
-        var split2 = split[1].split("];");
-        return split2[0] + "]";
-    }
-    private String cleanJsonToMakeItArray(String finalString, Integer amount){
-        //Evitar esto luego
-
-        for(int i = 0; i< amount +1;i++){
-            finalString = finalString.replaceFirst("\""+i +"\":","");
-        }
-
-
-
-        finalString = finalString.substring(1, finalString.length() -1);
-
-        finalString = "[" + finalString + "]";
-        return finalString;
+        var split2 = split[1].split("};");
+        return split2[0].concat("}").split("]")[0];
     }
 
     public void changeAmount(long value) {
         searchValue = (int)value;
+        System.out.println(searchValue);
     }
 }
