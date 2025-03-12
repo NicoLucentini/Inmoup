50e4c718ca29f540dfdd2efa3fd23b2b56912c96
NicoLucentini <lucentini.nicolas@gmail.com>
2025-03-12 17:49:49 -0300

    heap controller

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -2,6 +2,7 @@ package org.example.controller;
 
 import org.example.services.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,16 @@ public class MainController {
     public void hola(){
        String res =  mainService.hola();
        System.out.println(res);
+    }
+    @GetMapping("/heap")
+    public ResponseEntity<String> heap(){
+        long heapSize = Runtime.getRuntime().totalMemory() / (1024 * 1024);
+        long maxHeapSize = Runtime.getRuntime().maxMemory() / (1024 * 1024);
+        String heap="Heap Size (MB): " + heapSize;
+        String maxHeap="Max Heap Size (MB): " + maxHeapSize;
+        System.out.println(heap);
+        System.out.println(maxHeap);
+        return ResponseEntity.ok(heap + " | " + maxHeap);
     }
 
 }
