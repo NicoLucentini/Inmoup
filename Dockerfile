df6d19f860c82567a5aad1068ee6b7c8a194cbde
NicoLucentini <lucentini.nicolas@gmail.com>
2025-03-12 16:38:42 -0300

    more memory maybe

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:22-jdk-oracle
 
+ENV JAVA_OPTS="-Xms1g -Xmx2g"
 COPY target/BasicSpringApp-1.0-SNAPSHOT.jar app.jar
 
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,6 +14,11 @@ public class Main {
     public static void main(String[] args) {
 
         SpringApplication.run(Main.class, args);
+        long heapSize = Runtime.getRuntime().totalMemory() / (1024 * 1024);
+        long maxHeapSize = Runtime.getRuntime().maxMemory() / (1024 * 1024);
+
+        System.out.println("Heap Size (MB): " + heapSize);
+        System.out.println("Max Heap Size (MB): " + maxHeapSize);
     }
 
 }
